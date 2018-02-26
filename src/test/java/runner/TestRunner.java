f1895c617ffb0632b6fe5d62c8f538d6795f600b
sophieaviles <saviles@liveopscloud.com>
2018-02-26 13:58:37 -0600

    fourth commit

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -8,6 +8,6 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(features = {"src/test/java/features"}, format = {"json:target/Reports/cucumber.json", "html:target/Reports/cucumber-html-report"}, glue ="Steps" )
 public class TestRunner {
-//new info
-//prueba
+
+    
 }
