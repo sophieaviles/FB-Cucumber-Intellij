df87c83f64dbe32d03f4b8686767c11462f6be2b
Sofia Aviles <saviles@liveopscloud.com>
2018-02-26 13:36:19 -0600

    second commit

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -9,5 +9,5 @@ import org.junit.runner.RunWith;
 @CucumberOptions(features = {"src/test/java/features"}, format = {"json:target/Reports/cucumber.json", "html:target/Reports/cucumber-html-report"}, glue ="Steps" )
 public class TestRunner {
 
-
+//prueba
 }
