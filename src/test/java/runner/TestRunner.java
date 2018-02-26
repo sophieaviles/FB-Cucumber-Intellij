89c58d1b036f0e32fe7de760c5fc3c81485be119
Sofia Aviles <saviles@liveopscloud.com>
2018-02-26 13:30:11 -0600

    First commit

---
+++ src/test/java/runner/TestRunner.java
@@ -0,0 +1,13 @@
+package runner;
+
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = {"src/test/java/features"}, format = {"json:target/Reports/cucumber.json", "html:target/Reports/cucumber-html-report"}, glue ="Steps" )
+public class TestRunner {
+
+
+}
